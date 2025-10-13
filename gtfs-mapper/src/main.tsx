c1c74d8891bdd09cbe9421c4c6d555962086a383
Özenç Bilgili <alvaro.suarez.trabanco@gmail.com>
2025-10-13 10:52:37 +0200

    mapper organized better

--- gtfs-mapper/src/main.tsx
+++ gtfs-mapper/src/main.tsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { createRoot } from "react-dom/client";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "leaflet/dist/leaflet.css"; // <-- IMPORTANT
 
+const root = createRoot(document.getElementById("root")!);
+root.render(<App />);
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
